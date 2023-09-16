b85090b09c2b8dc3563528c95177b5df5097c7ee
MistofThunder27 <137645063+MistofThunder27@users.noreply.github.com>
2023-09-16 20:57:18 +0200

    Implemented object colour and radius

--- code_directory/2D_no_dependancies.py
+++ code_directory/2D_no_dependancies.py
@@ -1,104 +1,105 @@
 import math
 import tkinter
 
-# important variables
+# Reference values
+G = 6.67408e-11
+max_object_num = 10
+# Default settings
 delta_time = 5
 scale = 1
 x_avg = 0
 y_avg = 0
-# Constant
-G = 6.67408e-11
-# store the global state as a dictionary. Each entry in this dictionary is as follows:
-# dictionary = {obj_name: [obj_mass, obj_x_pos, obj_y_pos, obj_x_vel, obj_y_vel]}
-global_state = {}
+history_record_length = 10
+centre_mode = "geometrical"
+do_not_scale = False  # When False recalculate scale, otherwise use the global. The name shadowing is INTENTIONAL
 
+# store the global state as a dictionary. Each entry in this dictionary is as follows:
+# dictionary = {obj_name: [[obj_mass, obj_x_pos, obj_y_pos, obj_x_vel, obj_y_vel], "colour", radius]}
+global_state = {}
 # store the last 10 positions of each object in a dictionary
 location_history = {}
 
 # Test situation
 test_start_state = {
-    "obj1": [1e10, 10, 0, 0, 0],
-    "obj2": [1e10, -10, 0, 0, 0],
-    "obj3": [1e10, 0, 10, 0, -0.0125],
+    "obj1": [[1e10, 10, 0, 0, 0], "red", 1],
+    "obj2": [[1e10, -10, 0, 0, 0], "green", 1],
+    "obj3": [[1e10, 0, 10, 0, -0.0125], "blue", 1]
 }
 global_state = test_start_state
 
 
+# The following function takes the current frame information state and calculates the next
 def update_state():
-    # The following function takes the current frame information state and calculates the next
-    def calculate_next_frame(last_frame_state):
-        print("CALC NEW FRAME ---------------------------------------")
-        new_frame_state = {}
-        for object1_name in last_frame_state:
-            print(f"main object: {object1_name}")
-            object1_properties = last_frame_state[object1_name]
-            print("properties", object1_properties)
-            obj1_mass, obj1_x_pos, obj1_y_pos, obj1_x_vel, obj1_y_vel = object1_properties
-            # Calculate new position = old position + old velocity * delta time
-            new_x_pos = obj1_x_pos + obj1_x_vel * delta_time
-            print("new_x_pos", new_x_pos)
-            new_y_pos = obj1_y_pos + obj1_y_vel * delta_time
-            print("new_y_pos", new_y_pos)
-            # store the position history
-            obj_pos_his = location_history.setdefault(object1_name, [])
-            if len(obj_pos_his) > 9:
-                obj_pos_his.pop(0)
-            obj_pos_his.append([new_x_pos, new_y_pos])
-            print("loc his", location_history)
-
-            # Calculate acceleration
-            x_acc = 0
-            y_acc = 0
-            for object2_name in last_frame_state:
-                if object1_name != object2_name:
-                    print(f"reference object: {object2_name}")
-                    object2_properties = last_frame_state[object2_name]
-                    print("properties", object2_properties)
-                    obj2_x_pos, obj2_y_pos = object2_properties[1], object2_properties[2]
-                    # the gravitational force per unit mass formula is Gm/r**2 in the direction of the object
-                    # calculate the force by using the formula while taking the object2 as the center
-                    delta_x = obj1_x_pos - obj2_x_pos
-                    print("delta-x", delta_x)
-                    delta_y = obj1_y_pos - obj2_y_pos
-                    print("delta_y", delta_y)
-                    acc_value = G * obj1_mass / (delta_y**2 + delta_x**2)
-                    print("acc_value", acc_value)
-                    # now the angle
-                    try:
-                        angle = math.atan(delta_y/delta_x)
-                    except ZeroDivisionError:
-                        angle = math.pi/2 * (1 if delta_y > 0 else -1)
-                    if delta_x < 0:  # to account for the full 360 degrees
-                        angle += math.pi
-                    print("angle", angle)
-                    # now the components, the negative sign is because gravity is an attractive force
-                    x_acc += -acc_value * math.cos(angle)
-                    print("x_acc", x_acc)
-                    y_acc += -acc_value * math.sin(angle)
-                    print("y_acc", y_acc)
-
-            # Calculate new velocity = old velocity + acceleration * delta time
-            new_x_vel = obj1_x_vel + x_acc * delta_time
-            print("new_x_vel", new_x_vel)
-            new_y_vel = obj1_y_vel + y_acc * delta_time
-            print("new_y_vel", new_y_vel)
-
-            new_frame_state[object1_name] = [obj1_mass, new_x_pos, new_y_pos, new_x_vel, new_y_vel]
-
-        print(new_frame_state)
-        return new_frame_state
-
     global global_state
-    global_state = calculate_next_frame(global_state)
+    print("CALC NEW FRAME ---------------------------------------")
+    new_frame_state = {}
+    for object1_name in global_state:
+        print(f"main object: {object1_name}")
+        object1_properties = global_state[object1_name][0]
+        print("properties", object1_properties)
+        obj1_mass, obj1_x_pos, obj1_y_pos, obj1_x_vel, obj1_y_vel = object1_properties
+        # Calculate new position = old position + old velocity * delta time
+        new_x_pos = obj1_x_pos + obj1_x_vel * delta_time
+        print("new_x_pos", new_x_pos)
+        new_y_pos = obj1_y_pos + obj1_y_vel * delta_time
+        print("new_y_pos", new_y_pos)
+        # store the position history
+        obj_pos_his = location_history.setdefault(object1_name, [])
+        if len(obj_pos_his) > history_record_length - 1:
+            obj_pos_his.pop(0)
+        obj_pos_his.append([new_x_pos, new_y_pos])
+        print("loc his", location_history)
+
+        # Calculate acceleration
+        x_acc = 0
+        y_acc = 0
+        for object2_name in global_state:
+            if object1_name != object2_name:
+                print(f"reference object: {object2_name}")
+                object2_properties = global_state[object2_name][0]
+                print("properties", object2_properties)
+                obj2_x_pos, obj2_y_pos = object2_properties[1], object2_properties[2]
+                # the gravitational force per unit mass formula is Gm/r**2 in the direction of the object
+                # calculate the force by using the formula while taking the object2 as the center
+                delta_x = obj1_x_pos - obj2_x_pos
+                print("delta-x", delta_x)
+                delta_y = obj1_y_pos - obj2_y_pos
+                print("delta_y", delta_y)
+                acc_value = G * obj1_mass / (delta_y ** 2 + delta_x ** 2)
+                print("acc_value", acc_value)
+                # now the angle
+                try:
+                    angle = math.atan(delta_y / delta_x)
+                except ZeroDivisionError:
+                    angle = math.pi / 2 * (1 if delta_y > 0 else -1)
+                if delta_x < 0:  # to account for the full 360 degrees
+                    angle += math.pi
+                print("angle", angle)
+                # now the components, the negative sign is because gravity is an attractive force
+                x_acc += -acc_value * math.cos(angle)
+                print("x_acc", x_acc)
+                y_acc += -acc_value * math.sin(angle)
+                print("y_acc", y_acc)
+
+        # Calculate new velocity = old velocity + acceleration * delta time
+        new_x_vel = obj1_x_vel + x_acc * delta_time
+        print("new_x_vel", new_x_vel)
+        new_y_vel = obj1_y_vel + y_acc * delta_time
+        print("new_y_vel", new_y_vel)
+
+        new_frame_state[object1_name] = [[obj1_mass, new_x_pos, new_y_pos, new_x_vel, new_y_vel],
+                                         global_state[object1_name][1], global_state[object1_name][-1]]
+    print(new_frame_state)
+    global_state = new_frame_state
 
 
 def update_display():
     print("UPDATE DISPLAY ---------------------------------------------------")
-    global global_state, main_display, x_avg, y_avg, scale
-    radius = 1
-    # Find centre point and scale
+    global global_state, main_display
+    # Find "geometrical" centre point and scale
+    largest_radius = 0
     for object_name in global_state:
-        object_properties = global_state[object_name]
+        object_properties = global_state[object_name][0]
         obj_x_pos = object_properties[1]
         obj_y_pos = object_properties[2]
         try:
@@ -109,23 +110,33 @@ def update_display():
         except UnboundLocalError:
             x_max = x_min = obj_x_pos
             y_max = y_min = obj_y_pos
+        largest_radius = max(largest_radius, global_state[object_name][-1])
     print("min-max", x_max, x_min, y_max, y_min)
-    x_avg = (x_max + x_min)/len(global_state)
-    y_avg = (y_max + y_min)/len(global_state)
+    if centre_mode == "origin":
+        global x_avg, y_avg
+    elif centre_mode == "geometrical":
+        x_avg = (x_max + x_min) / len(global_state)
+        y_avg = (y_max + y_min) / len(global_state)
+    elif centre_mode == "centre of mass":
+        pass  # TODO: IMPLEMENT
     main_display.update()
     win_height = main_display.winfo_height()
     win_width = main_display.winfo_width()
     print("win", win_height, win_width)
-    # The 2*radius is so than 2 circles at each end would be in frame
-    scale = min(win_width/(x_max - x_min + 2*radius + 10), win_height/(y_max - y_min + 2*radius + 10))
+    if do_not_scale:
+        global scale
+    else:
+        # The 2*radius is so than 2 circles at each end would be in frame
+        scale = min(win_width / (x_max - x_min + 2 * largest_radius + 10),
+                    win_height / (y_max - y_min + 2 * largest_radius + 10))
     print("centre point", x_avg, y_avg)
     print("scale", scale)
 
     # draw to canvas
     main_display.delete("all")
     # Find the zero line axes
-    zero_y_coord = win_height/2 + y_avg * scale  # the negative is because in Tkinter y increases downwards
-    zero_x_coord = win_width/2 - x_avg * scale
+    zero_y_coord = win_height / 2 + y_avg * scale  # the negative is because in Tkinter y increases downwards
+    zero_x_coord = win_width / 2 - x_avg * scale
     print("zero coords", zero_x_coord, zero_y_coord)
     # Draw them
     main_display.create_line(0, zero_y_coord, win_width, zero_y_coord)  # x-axis
@@ -136,22 +147,25 @@ def update_display():
         for i in range(len(object_history) - 1):
             x0 = (object_history[i][0] - x_avg) * scale + win_width / 2
             y0 = win_height / 2 - (object_history[i][1] - y_avg) * scale
-            x1 = (object_history[i+1][0] - x_avg) * scale + win_width / 2
-            y1 = win_height / 2 - (object_history[i+1][1] - y_avg) * scale
+            x1 = (object_history[i + 1][0] - x_avg) * scale + win_width / 2
+            y1 = win_height / 2 - (object_history[i + 1][1] - y_avg) * scale
             print("line coord", x0, y0, x1, y1)
             main_display.create_line(x0, y0, x1, y1, fill="red")
     # Draw objects
     for object_name in global_state:
-        object_properties = global_state[object_name]
+        print(object_name)
+        object_properties = global_state[object_name][0]
+        object_colour = global_state[object_name][1]
+        radius = global_state[object_name][-1]
         obj_x_rel_pos = object_properties[1] - x_avg
         obj_y_rel_pos = object_properties[2] - y_avg
         print("rel pos", obj_x_rel_pos, obj_y_rel_pos)
-        x0 = (obj_x_rel_pos - radius)*scale + win_width/2  # left-most point
-        x1 = (obj_x_rel_pos + radius)*scale + win_width/2  # right-most point
-        y0 = win_height/2 - (obj_y_rel_pos - radius)*scale  # bottom-most point (least negative y as tkinter increases y downwards)
-        y1 = win_height/2 - (obj_y_rel_pos + radius)*scale  # top-most point (most negative y as tkinter decreases y downwards)
+        x0 = (obj_x_rel_pos - radius) * scale + win_width / 2  # left-most point
+        x1 = (obj_x_rel_pos + radius) * scale + win_width / 2  # right-most point
+        y0 = win_height / 2 - (obj_y_rel_pos + radius) * scale  # bottom-most point
+        y1 = win_height / 2 - (obj_y_rel_pos - radius) * scale  # top-most point
         print(x0, x1, y0, y1)
-        main_display.create_oval(x0, y0, x1, y1, fill="red")
+        main_display.create_oval(x0, y0, x1, y1, fill=object_colour)
 
 
 def main_update():
@@ -159,16 +173,33 @@ def main_update():
     update_display()
 
 
+def toggle_settings():
+    global settings_toggle, settings_frame
+    if settings_toggle:
+        settings_toggle = False
+        settings_frame.pack_forget()
+    else:
+        settings_toggle = True
+        settings_frame.pack(side="right", fill="both", expand=True)
+    update_display()
+    print("settings_toggle", settings_toggle)
+
+
 main_window = tkinter.Tk()
 main_window.geometry("1000x600+100+100")
 main_window.title("2D simulation of the Newtonian model of gravity")
 
-next_frame_button = tkinter.Button(main_window, text="Next frame", command=main_update)
-next_frame_button.pack(side="bottom", fill="x")
+button_frame = tkinter.Frame(main_window)
+button_frame.pack(side="bottom", fill="x")
+settings_toggle = False
+settings_button = tkinter.Button(button_frame, text="Settings", command=toggle_settings)
+settings_button.pack(side="right", fill="x")
+next_frame_button = tkinter.Button(button_frame, text="Next frame", command=main_update)
+next_frame_button.pack(side="left", fill="x", expand=True)
 
 main_display = tkinter.Canvas(main_window)
-main_display.pack(fill="both", expand=True)
-
+main_display.pack(side="left", fill="both", expand=True)
+settings_frame = tkinter.Frame(main_window)
 
 update_display()
 main_window.mainloop()
