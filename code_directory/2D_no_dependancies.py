8ff1a0bf60a2c57fa7b825a347bb1122444abe7d
MistofThunder27 <137645063+MistofThunder27@users.noreply.github.com>
2023-09-23 13:57:24 +0200

    added centring options

--- code_directory/2D_no_dependancies.py
+++ code_directory/2D_no_dependancies.py
@@ -6,11 +6,8 @@ G = 6.67408e-11
 max_object_num = 10
 # Default settings
 delta_time = 10000
-scale = 1
-x_avg = 0
-y_avg = 0
 history_record_length = 100
-centre_mode = "geometrical"
+centre_reference = "geometrical"
 absolute_scale = False  # When False recalculate scale, otherwise use the global. The name shadowing is INTENTIONAL
 
 # store the global state as a dictionary. Each entry in this dictionary is as follows:
@@ -21,18 +18,25 @@ global_state = {}
 location_history = {}
 
 # Test cases
-test_case_1 = {  # Earth-Moon-sun system (recommended delta_time 10000)
+test_case_1 = {  # Solar system (recommended delta_time 10000)
     "Sun": [1.989e30, [0, 0, 0, 0], "yellow", 10000000000],
+    "Mercury": [3.301e23, [5.8e10, 0, 0, 47000], "brown", 300000000],
+    "Venus": [4.867e24, [1.08e11, 0, 0, 35000], "brown", 300000000],
     "Earth": [5.9722e24, [1.5037e11, 0, 0, 29780], "blue", 300000000],
     "Moon": [7.34767e22, [1.507544e11, 0, 0, 30802], "grey", 100000000]
 }
 test_case_2 = {  # direct collision (recommended delta_time 5)
     "Obj1": [1e10, [10, 0, 0, 0], "red", 1],
-    "Obj2": [1e10, [-10, 0, 0, 0], "blue", 1],
+    "Obj2": [1e10, [-10, 0, 0, 0], "blue", 1]
 }
 test_case_3 = {  # double orbit (recommended delta_time 5)
     "Obj1": [1e10, [20, 0, 0, -0.08], "red", 1],
+    "Obj2": [1e10, [-20, 0, 0, 0.08], "blue", 1]
+}
+test_case_4 = {  # three objects (recommended delta_time 5)
+    "Obj1": [1e10, [20, 0, 0, -0.08], "red", 1],
     "Obj2": [1e10, [-20, 0, 0, 0.08], "blue", 1],
+    "Obj3": [1e10, [0, -20, 0.01, 0], "yellow", 1]
 }
 
 global_state = test_case_1
@@ -67,18 +71,18 @@ def update_state():
                 print("delta_y", delta_y)
                 acc_value = G * global_state[object2_name][0] / (delta_y ** 2 + delta_x ** 2)
                 print("acc_value", acc_value)
-                # now the angle
+                # now the angle. As gravity is attractive I will calculate the angle flipped 180 degrees
                 try:
                     angle = math.atan(delta_y / delta_x)
+                    if delta_x > 0:  # to account for the full 360 degrees
+                        angle += math.pi
                 except ZeroDivisionError:
-                    angle = math.pi / 2 * (1 if delta_y > 0 else -1)
-                if delta_x < 0:  # to account for the full 360 degrees
-                    angle += math.pi
+                    angle = math.pi / 2 * (1 if delta_y < 0 else -1)
                 print("angle", angle)
                 # now the components, the negative sign is because gravity is an attractive force
-                x_acc += -acc_value * math.cos(angle)
+                x_acc += acc_value * math.cos(angle)
                 print("x_acc", x_acc)
-                y_acc += -acc_value * math.sin(angle)
+                y_acc += acc_value * math.sin(angle)
                 print("y_acc", y_acc)
 
         # Calculate new velocity = old velocity + acceleration * delta time
@@ -95,58 +99,137 @@ def update_state():
         while len(obj_pos_his) > history_record_length - 1:
             obj_pos_his.pop(0)
         obj_pos_his.append([new_x_pos, new_y_pos])
-        print("loc his", location_history)
+        print("loc his", obj_pos_his)
 
-    print(new_frame_state)
+    # save as the new global state
     global_state = new_frame_state
+    print("new global state", global_state)
 
 
 def update_display():
-    print("UPDATE DISPLAY ---------------------------------------------------")
-    global main_display
-    # Find "geometrical" centre point and scale
-    object_names = list(global_state.keys())
-    # set starting x, y and radius values for this calculation
-    first_object_name = object_names.pop(0)
-    x_max = x_min = global_state[first_object_name][1][0]
-    y_max = y_min = global_state[first_object_name][1][1]
-    largest_radius = global_state[first_object_name][-1]
-    # now compare with the rest. Remember that we popped the first
-    for object_name in object_names:
-        obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
-        x_max = max(x_max, obj_x_pos)
-        x_min = min(x_min, obj_x_pos)
-        y_max = max(y_max, obj_y_pos)
-        y_min = min(y_min, obj_y_pos)
-        largest_radius = max(largest_radius, global_state[object_name][-1])
-    print("min-max", x_max, x_min, y_max, y_min)
-    print("largest radius", largest_radius)
-    if centre_mode == "origin":
-        global x_avg, y_avg
-    elif centre_mode == "geometrical":
-        x_avg = (x_max + x_min) / 2
-        y_avg = (y_max + y_min) / 2
-    elif centre_mode == "centre of mass":
-        pass  # TODO: IMPLEMENT
+    print("UPDATE DISPLAY ---------------------------------------------------")  # TODO: EVERYTHING
+    global main_display, centre_reference
     main_display.update()
     win_height = main_display.winfo_height()
     win_width = main_display.winfo_width()
     print("win", win_height, win_width)
-    if absolute_scale:
-        global scale
-    else:
-        # The 3*radius is so that 2 circles at each end would be in frame and
-        # an extra half radius on both sides as padding
-        scale = min(win_width / (x_max - x_min + 3 * largest_radius),
-                    win_height / (y_max - y_min + 3 * largest_radius))
-    print("centre point", x_avg, y_avg)
+
+    if centre_reference in global_state:
+        print("centre:", centre_reference)
+        centre_x, centre_y = global_state[centre_reference][1][:2]
+        furthest_x = furthest_y = 0
+        furthest_x_name = furthest_y_name = centre_reference
+        for object_name in global_state:
+            if object_name is not centre_reference:  # TODO: is this check necessary?
+                obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
+                if math.fabs(obj_x_pos - centre_x) > furthest_x:
+                    furthest_x_name = object_name
+                    furthest_x = math.fabs(obj_x_pos - centre_x)
+                if math.fabs(obj_y_pos - centre_y) > furthest_y:
+                    furthest_y_name = object_name
+                    furthest_y = math.fabs(obj_y_pos - centre_y)
+        print(f"furthest objects: X-{furthest_x_name} at a distance of {furthest_x} and "
+              f"Y-{furthest_y_name} at a distance of {furthest_y}")
+
+        if absolute_scale:
+            scale = 1
+        else:
+            # The 2* is so that it covers the same distance on both sides
+            scale = min(win_width / (2 * (furthest_x + global_state[furthest_x_name][-1])),
+                        win_height / (2 * (furthest_y + global_state[furthest_y_name][-1])))
+
+    elif centre_reference == "origin":
+        print("centre: origin")
+        centre_x = centre_y = furthest_x = furthest_y = 0
+        for object_name in global_state:
+            obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
+            if math.fabs(obj_x_pos) > furthest_x:
+                furthest_x_name = object_name
+                furthest_x = math.fabs(obj_x_pos)
+            if math.fabs(obj_y_pos) > furthest_y:
+                furthest_y_name = object_name
+                furthest_y = math.fabs(obj_y_pos)
+        print("furthest points at", furthest_x, furthest_y)
+
+        if absolute_scale:
+            scale = 1
+        else:
+            # The 2* is so that it covers the same distance on both sides
+            scale = min(win_width / (2 * (furthest_x + global_state.get(furthest_x_name, [0])[-1])),
+                        win_height / (2 * (furthest_y + global_state.get(furthest_y_name, [0])[-1])))
+
+    elif centre_reference == "geometrical":
+        print("centre: geometrical")
+        # Find "geometrical" centre point
+        object_names = list(global_state.keys())
+        # set starting x, y and radius values for this calculation
+        first_object_name = object_names.pop(0)
+        x_max = x_min = global_state[first_object_name][1][0]
+        y_max = y_min = global_state[first_object_name][1][1]
+        largest_radius = global_state[first_object_name][-1]
+        # now compare with the rest. Remember that we popped the first
+        for object_name in object_names:
+            obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
+            x_max = max(x_max, obj_x_pos)
+            x_min = min(x_min, obj_x_pos)
+            y_max = max(y_max, obj_y_pos)
+            y_min = min(y_min, obj_y_pos)
+            largest_radius = max(largest_radius, global_state[object_name][-1])  # TODO: radius of furthest?
+        print("min-max", x_max, x_min, y_max, y_min)
+        print("largest radius", largest_radius)
+        centre_x = (x_max + x_min) / 2
+        centre_y = (y_max + y_min) / 2
+
+        if absolute_scale:
+            scale = 1
+        else:
+            # The 3*radius is so that 2 circles at each end would be in frame and
+            # an extra half radius on both sides as padding
+            scale = min(win_width / (x_max - x_min + 2 * largest_radius),
+                        win_height / (y_max - y_min + 2 * largest_radius))
+
+    else:  # if centre_reference == "centre of mass":
+        print("centre: mass")
+        # Find centre of mass
+        centre_x = 0
+        centre_y = 0
+        sum_of_masses = 0
+        for object_name in global_state:
+            obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
+            obj_mass = global_state[object_name][0]
+            centre_x += obj_mass * obj_x_pos
+            centre_y += obj_mass * obj_y_pos
+            sum_of_masses += obj_mass
+        centre_x = centre_x / sum_of_masses
+        centre_y = centre_y / sum_of_masses
+
+        # Then find the "min"s and "max"es
+        furthest_x = furthest_y = 0
+        for object_name in global_state:
+            obj_x_pos, obj_y_pos = global_state[object_name][1][:2]
+            if math.fabs(obj_x_pos - centre_x) > furthest_x:
+                furthest_x_name = object_name
+                furthest_x = math.fabs(obj_x_pos - centre_x)
+            if math.fabs(obj_y_pos - centre_y) > furthest_y:
+                furthest_y_name = object_name
+                furthest_y = math.fabs(obj_y_pos - centre_y)
+
+        if absolute_scale:
+            scale = 1
+        else:
+            # The 2* is so that it covers the same distance on both sides
+            scale = min(win_width / (2 * (furthest_x + global_state.get(furthest_x_name, [0])[-1])),
+                        win_height / (2 * (furthest_y + global_state.get(furthest_y_name, [0])[-1])))
+
+    print("centre point", centre_x, centre_y)
     print("scale", scale)
 
     # draw to canvas
     main_display.delete("all")
-    # Find the zero line axes
-    zero_y_coordinate = win_height / 2 + y_avg * scale  # the positive y_avg is because in Tkinter y increases downwards
-    zero_x_coordinate = win_width / 2 - x_avg * scale
+    # Find the zero line axes  #TODO: add support for grids
+    # the positive centre_y is because in Tkinter y increases downwards
+    zero_y_coordinate = win_height / 2 + centre_y * scale
+    zero_x_coordinate = win_width / 2 - centre_x * scale
     print("zero coordinates", zero_x_coordinate, zero_y_coordinate)
     # Draw them
     main_display.create_line(0, zero_y_coordinate, win_width, zero_y_coordinate)  # x-axis
@@ -156,10 +239,9 @@ def update_display():
         # Draw objects
         print(object_name)
         object_numbers = global_state[object_name][1][:2]
-        object_colour = global_state[object_name][-2]
-        radius = global_state[object_name][-1]
-        obj_x_rel_pos = object_numbers[0] - x_avg
-        obj_y_rel_pos = object_numbers[1] - y_avg
+        object_colour, radius = global_state[object_name][-2:]
+        obj_x_rel_pos = object_numbers[0] - centre_x
+        obj_y_rel_pos = object_numbers[1] - centre_y
         print("rel pos", obj_x_rel_pos, obj_y_rel_pos)
         x0 = (obj_x_rel_pos - radius) * scale + win_width / 2  # left-most point
         x1 = (obj_x_rel_pos + radius) * scale + win_width / 2  # right-most point
@@ -171,12 +253,12 @@ def update_display():
         # Draw path from location history
         object_history = location_history[object_name]
         print("obj his", object_history)
-        x0 = (object_history[0][0] - x_avg) * scale + win_width / 2
-        y0 = win_height / 2 - (object_history[0][1] - y_avg) * scale
+        x0 = (object_history[0][0] - centre_x) * scale + win_width / 2
+        y0 = win_height / 2 - (object_history[0][1] - centre_y) * scale
         print("starting coordinate in line", x0, y0)
         for i in range(1, len(object_history)):
-            x1 = (object_history[i][0] - x_avg) * scale + win_width / 2
-            y1 = win_height / 2 - (object_history[i][1] - y_avg) * scale
+            x1 = (object_history[i][0] - centre_x) * scale + win_width / 2
+            y1 = win_height / 2 - (object_history[i][1] - centre_y) * scale
             print("next coordinate in line", x1, y1)
             main_display.create_line(x0, y0, x1, y1, fill=object_colour)
             x0, y0 = x1, y1
@@ -229,7 +311,9 @@ settings_frame = tkinter.Frame(main_window)
 
 # test settings:
 # absolute_scale = True
-# centre_mode = "zero"
+# centre_reference = "origin"
+# centre_reference = "mass"
+# centre_reference = "Obj1"
 
 update_display()
 main_window.mainloop()
